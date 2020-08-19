93ad433e95ecdae927813d90fa818b1e1e895fe7
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-08-19 11:36:13 +0800

    用php改寫畫面

--- Lab_MySQL_basic_1_start.php
+++ Lab_MySQL_basic_1_start.php
@@ -5,24 +5,27 @@ header("content-type:text/html; charset=utf-8");
 
 
 // 1. 連接資料庫伺服器
-// $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-// $result = mysqli_query($link, "set names utf8");
-// mysqli_select_db($link, "class");
+$link = @mysqli_connect("localhost", "root", "root") or die;
+
+$result = mysqli_query($link, "set names utf8");
+mysqli_select_db($link, "class");
 
 // 2. 執行 SQL 敘述
-// $commandText = "select * from students";
-// $result = mysqli_query($link, $commandText);
-
+$commandText = "select * from students";
+$result = mysqli_query($link, $commandText);
+// (var_dump($result));
 // 3. 處理查詢結果
-// while ($row = mysqli_fetch_assoc($result))
-// {
-//   echo "ID：{$row['cID']}<br>";
-//   echo "Name：{$row['cName']}<br>";
-//   echo "<HR>";
-// }
+while ($row = mysqli_fetch_assoc($result))
+{
+    // $result = mysqli_query($link, $commandText);
+    echo $row["cName"];
+  echo "ID：$row[cID]<br>";
+  echo "Name：{$row['cName']}<br>";
+  echo "<HR>";
+}
 
 // 4. 結束連線
-// mysqli_close($link);
+mysqli_close($link);
 
 echo "<br />-- Done --";
 ?>
